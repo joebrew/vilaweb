11b2b88ebba256546b04a5be4d6844adca68bf62
Joe Brew <joebrew@gmail.com>
2018-12-19 17:45:03 +0100

    update

---
+++ inst/rmd/newspapers/README.md
@@ -0,0 +1,29 @@
+Newspapers
+================
+
+The question
+============
+
+Politicians often tweet about newspapers, either by directly linking to a story or by re-tweeting a newspaper's tweet of a story. But politicians tend to link to stories favorable to their worldview. What is the distribution of newspaper article retweets/links among politicians
+
+The methods
+===========
+
+We gathered 786973 tweets from 54 well-known Catalan and Spanish politicians. We algorithmically identified references to 41 newspapers, and calculated the percentage of references to each for each politician.
+
+Results
+=======
+
+By politician
+-------------
+
+The below shows the top 10 newspaper re-tweets for each politician.
+
+![](figures/unnamed-chunk-3-1.png)![](figures/unnamed-chunk-3-2.png)![](figures/unnamed-chunk-3-3.png)![](figures/unnamed-chunk-3-4.png)![](figures/unnamed-chunk-3-5.png)![](figures/unnamed-chunk-3-6.png)![](figures/unnamed-chunk-3-7.png)![](figures/unnamed-chunk-3-8.png)![](figures/unnamed-chunk-3-9.png)![](figures/unnamed-chunk-3-10.png)![](figures/unnamed-chunk-3-11.png)![](figures/unnamed-chunk-3-12.png)![](figures/unnamed-chunk-3-13.png)![](figures/unnamed-chunk-3-14.png)![](figures/unnamed-chunk-3-15.png)![](figures/unnamed-chunk-3-16.png)![](figures/unnamed-chunk-3-17.png)![](figures/unnamed-chunk-3-18.png)![](figures/unnamed-chunk-3-19.png)![](figures/unnamed-chunk-3-20.png)![](figures/unnamed-chunk-3-21.png)![](figures/unnamed-chunk-3-22.png)![](figures/unnamed-chunk-3-23.png)![](figures/unnamed-chunk-3-24.png)![](figures/unnamed-chunk-3-25.png)![](figures/unnamed-chunk-3-26.png)![](figures/unnamed-chunk-3-27.png)![](figures/unnamed-chunk-3-28.png)![](figures/unnamed-chunk-3-29.png)![](figures/unnamed-chunk-3-30.png)![](figures/unnamed-chunk-3-31.png)![](figures/unnamed-chunk-3-32.png)![](figures/unnamed-chunk-3-33.png)![](figures/unnamed-chunk-3-34.png)![](figures/unnamed-chunk-3-35.png)![](figures/unnamed-chunk-3-36.png)![](figures/unnamed-chunk-3-37.png)![](figures/unnamed-chunk-3-38.png)![](figures/unnamed-chunk-3-39.png)![](figures/unnamed-chunk-3-40.png)![](figures/unnamed-chunk-3-41.png)![](figures/unnamed-chunk-3-42.png)![](figures/unnamed-chunk-3-43.png)![](figures/unnamed-chunk-3-44.png)![](figures/unnamed-chunk-3-45.png)![](figures/unnamed-chunk-3-46.png)![](figures/unnamed-chunk-3-47.png)![](figures/unnamed-chunk-3-48.png)![](figures/unnamed-chunk-3-49.png)![](figures/unnamed-chunk-3-50.png)![](figures/unnamed-chunk-3-51.png)![](figures/unnamed-chunk-3-52.png)![](figures/unnamed-chunk-3-53.png)![](figures/unnamed-chunk-3-54.png)
+
+By newspaper
+------------
+
+The below shows the top 10 re-tweeters for each newspaper (relative to each person's overall tweet-load).
+
+![](figures/unnamed-chunk-4-1.png)![](figures/unnamed-chunk-4-2.png)![](figures/unnamed-chunk-4-3.png)![](figures/unnamed-chunk-4-4.png)![](figures/unnamed-chunk-4-5.png)![](figures/unnamed-chunk-4-6.png)![](figures/unnamed-chunk-4-7.png)![](figures/unnamed-chunk-4-8.png)![](figures/unnamed-chunk-4-9.png)![](figures/unnamed-chunk-4-10.png)![](figures/unnamed-chunk-4-11.png)![](figures/unnamed-chunk-4-12.png)![](figures/unnamed-chunk-4-13.png)![](figures/unnamed-chunk-4-14.png)![](figures/unnamed-chunk-4-15.png)![](figures/unnamed-chunk-4-16.png)![](figures/unnamed-chunk-4-17.png)![](figures/unnamed-chunk-4-18.png)![](figures/unnamed-chunk-4-19.png)![](figures/unnamed-chunk-4-20.png)![](figures/unnamed-chunk-4-21.png)![](figures/unnamed-chunk-4-22.png)![](figures/unnamed-chunk-4-23.png)![](figures/unnamed-chunk-4-24.png)![](figures/unnamed-chunk-4-25.png)![](figures/unnamed-chunk-4-26.png)![](figures/unnamed-chunk-4-27.png)![](figures/unnamed-chunk-4-28.png)![](figures/unnamed-chunk-4-29.png)![](figures/unnamed-chunk-4-30.png)![](figures/unnamed-chunk-4-31.png)![](figures/unnamed-chunk-4-32.png)![](figures/unnamed-chunk-4-33.png)![](figures/unnamed-chunk-4-34.png)![](figures/unnamed-chunk-4-35.png)![](figures/unnamed-chunk-4-36.png)![](figures/unnamed-chunk-4-37.png)![](figures/unnamed-chunk-4-38.png)![](figures/unnamed-chunk-4-39.png)![](figures/unnamed-chunk-4-40.png)![](figures/unnamed-chunk-4-41.png)
--- inst/rmd/newspapers/get_tweets.R
+++ inst/rmd/newspapers/get_tweets.R
@@ -123,6 +123,9 @@ if('tl.RData' %in% dir()){
   save(tl, file = 'tl.RData')
 }
 
+tl <- tl %>% filter(username != 'xsalaimartin')
+tl <- tl %>% filter(!is.na(username))
+
 # Get the highest hosts
 hosts <- tl %>%
   group_by(host) %>%
@@ -132,23 +135,235 @@ hosts <- tl %>%
 
 
 
-make_keys <- function(x,y){
+make_keys <- function(x,y,z){
   return(data_frame(newspaper = x,
-                    key = y))
+                    site = y,
+                    handle = z))
 }
 # Extract core
 # Define the url keys
 keys <- 
   list(
-    make_keys('La Vanguardia', 'lavanguardia.com'),
-    make_keys('El Mundo', 'elmundo.es'),
-    make_keys('Telecinco', 'telecinco.es'),
-    make_keys('El Diario', 'eldiario.es'),
-    make_keys('Huffington Post', 'huffingtonpost.es'),
-    make_keys('El Público', 'publico.es'),
-    make_keys('El Periódico', 'elperiodico.es'),
-    make_keys('Cadana Ser', 'cadenaser.com'),
-    make_keys('La Verdad', 'laverdad.es'),
-    make_keys('20 Minutos', '20minutos.es')
+    make_keys('La Vanguardia', 'lavanguardia.com', 'lavanguardia'),
+    make_keys('El Mundo', 'elmundo.es', 'elmundoes'),
+    make_keys('Telecinco', 'telecinco.es', 'telecincoes'),
+    make_keys('El Diario', 'eldiario.es', 'eldiarioes'),
+    make_keys('Huffington Post', 'huffingtonpost.es',''),
+    make_keys('El Público', 'publico.es', 'publico'),
+    make_keys('El Periódico', 'elperiodico.es', 'elperiodico'),
+    make_keys('El Periódico', 'elperiodico.com', 'elperiodico'),
+    make_keys('Cadana Ser', 'cadenaser.com', 'cadanaser_espa'),
+    make_keys('La Verdad', 'laverdad.es', 'laverdad_es'),
+    make_keys('20 Minutos', '20minutos.es','20m'),
+    make_keys('El Español', 'elespanol.com', 'elespanolcom'),
+    make_keys('ABC', 'abc.es', 'abc_es'),
+    make_keys('Crónica Global', 'cronicaglobal.com', 'cronicaglobal'),
+    make_keys('CCMA', 'ccma.cat', 'ccmaa_cat'),
+    make_keys('RTVE', 'rtve.es', 'rtve'),
+    make_keys('ARA', 'ara.cat', 'diariaara'),
+    make_keys('El Confidencial', 'elconfidencial.com', 'elconfidencial'),
+    make_keys('Europa Press', 'europapress.es', 'europapress'),
+    make_keys('E-noticies', 'e-noticies.cat', 'enoticiescat'),
+    make_keys('E-noticies', 'e-noticies.es', 'enoticiescat'),
+    make_keys('VilaWeb', 'vilaweb.cat', 'VilaWeb'),
+    make_keys('Libertad Digital', 'libertaddigital.com', 'libertaddigital'),
+    make_keys('La Razón', 'larazon.es', 'larazon_es'),
+    make_keys('El Punt Avui', 'elpuntavui.cat', 'elpuntavui'),
+    make_keys('El Nacional', 'elnacional.cat', 'elnacionalcat'),
+    make_keys('Nació Digital', 'naciodigital.cat', 'naciodigital'),
+    make_keys('OKDiario', 'okdiario.com', 'okdiario'),
+    make_keys('Voz Populi', 'vozpopuli.com', 'voz_populi'),
+    make_keys('Onda Cero', 'ondacero.es', 'ondacero_es'),
+    make_keys('El Economista', 'eleconomista.es', 'eleconomistaes'),
+    make_keys('COPE', 'cope.es', 'cope'),
+    make_keys('Diari de Girona', 'diaridegirona.cat', 'diaridegirona'),
+    make_keys('elCatalán', 'elcatalan.es', 'elcatalan_es'),
+    make_keys('324 Cat', '324.cat', '324cat'),
+    make_keys('El País', 'elpais.com', 'elpais_espana'),
+    make_keys('El País', 'elpais.com', 'elpais'),
+    make_keys('El País', 'elpais.es', 'elpais_espana'),
+    make_keys('Economia Digital', 'economiadigital.es', 'economiaed_'),
+    make_keys('El Periódico', 'elperiodico.cat', 'elperiodico_cat'),
+    make_keys('Diari la Veu', 'diarilaveu.com', 'diarilaveu_'),
+    make_keys("L'independant", "lindependant.fr", "lindependant"),
+    make_keys("Le Monde", "lemonde.fr", "lemondefr"),
+    make_keys("Le Figaró", "lefigaro.fr", "le_figaro"),
+    make_keys("Diario de Mallorca", "diariodemallorca.es", "diariomallorca"),
+    make_keys("Última hora", "ultimahora.es", "uhmallorca"),
+    make_keys("Diari de Balears", "dbalears.cat", "dbalears")
   )
-# Get urls
+keys <- bind_rows(keys)
+
+# Manually combine some
+keys <- keys %>%
+  mutate(newspaper = ifelse(newspaper == 'Crónica Global',
+                            'El Español', 
+                            ifelse(newspaper == '324 Cat',
+                                   'CCMA/324',
+                                   newspaper)))
+
+# Flag the tweets
+tl$is_site <- !is.na(tl$host) & grepl('.', tl$host, fixed = TRUE)
+tl$is_handle <- !is.na(tl$host) & !grepl('.', tl$host, fixed = TRUE)
+
+# Lowercase all
+tl$host <- tolower(tl$host)
+keys$site <- tolower(keys$site)
+keys$handle <- tolower(keys$handle)
+
+# Keep only the data with newspaper references
+sites <- tl %>% filter(is_site)
+sites <- left_join(sites,
+                   keys %>% dplyr::select(newspaper, site),
+                   by = c('host' = 'site'))
+handles <- tl %>% filter(is_handle)
+handles <- left_join(handles,
+                   keys %>% dplyr::select(newspaper, handle),
+                   by = c('host' = 'handle'))
+df <- bind_rows(sites, handles)
+
+# Create plot
+create_plot <- function(language = 'en',
+                        usernames = NULL,
+                        n = NULL){
+  if(!is.null(usernames)){
+    plot_data <- 
+      df %>%
+      filter(username %in% usernames)
+  } else {
+    plot_data <- df
+  }
+  plot_data <- plot_data %>%
+    # mutate(newspaper = gsub(' ', '\n', newspaper)) %>%
+    mutate(person = username) %>%
+    dplyr::group_by(person,
+                    newspaper) %>%
+    tally %>%
+    ungroup %>%
+    group_by(person) %>%
+    mutate(p = n / sum(n) * 100) %>%
+    ungroup %>%
+    arrange(person,
+            desc(p)) %>%
+    dplyr::filter(!is.na(newspaper)) 
+  only_one <- length(usernames) == 1
+  if(only_one){
+    plot_data$newspaper <- factor(plot_data$newspaper,
+                                  levels = plot_data$newspaper)
+  }
+  if(!is.null(n)){
+    plot_data <- plot_data[1:n,]
+  }
+  
+  g <- ggplot(data = plot_data,
+         aes(x = newspaper,
+             y = p)) +
+    geom_bar(stat = 'identity',
+             alpha = 0.6,
+             fill = 'darkorange')
+  if(length(usernames) != 1){
+    g <- g + facet_wrap(~person)
+  } 
+  the_list <- 
+    paste0("20 Minutos, ABC, ARA, Cadana Ser, CCMA, CCMA/324, COPE, Diari de Balears, Diari de Girona, Diari la Veu, Diario de Mallorca,\nE-noticies, Economia Digital, El Confidencial, El Diario, El Economista, El Español, El Mundo, El Nacional, El País, \nEl Periódico, El Público, El Punt Avui, elCatalán, Europa Press, Huffington Post, L'independant, La Razón, La Vanguardia, \nLa Verdad, Le Figaró, Le Monde, Libertad Digital, Nació Digital, OKDiario, Onda Cero, RTVE, Telecinco, Última hora, VilaWeb, Voz Populi")
+  if(language == 'en'){
+    x <- 'Source'
+    title <- 'Distribution of newspaper references by politician (top 10)'
+    caption <- paste0('Includes the following only: ', the_list)
+  } else {
+    x <- 'Font'
+    title <- 'Distribució de referencies a diaris per polític (top 10)'
+    caption <- paste0('Només inclou: ', the_list)
+    
+  }
+  subtitle <- ifelse(only_one, paste0('@', plot_data$person[1]), '')
+  y <- 'Percentage'
+  
+  g <- g +
+    theme_vilaweb() +
+    labs(x = x, y = y, title = title, subtitle = subtitle, caption = caption) +
+    theme(plot.subtitle = element_text(size = 24, hjust = 0.5),
+          plot.caption = element_text(size = 7)) +
+    geom_text(aes(label = round(p, digits = 2)),
+               nudge_y = 0.3,
+              alpha = 0.6) +
+    theme(axis.text.x = element_text(angle = 90,
+                                     vjust = 0.5,
+                                     hjust = 1))
+  
+  return(g)
+}
+
+# Create plot
+create_plot_newspaper <- function(language = 'en',
+                        newspapers = NULL,
+                        n = NULL){
+  plot_data <- df
+  
+  plot_data <- plot_data %>%
+    # mutate(newspaper = gsub(' ', '\n', newspaper)) %>%
+    mutate(person = username) %>%
+    dplyr::group_by(person,
+                    newspaper) %>%
+    tally %>%
+    ungroup %>%
+    group_by(person) %>%
+    mutate(p = n / sum(n) * 100) %>%
+    ungroup %>%
+    arrange(person,
+            desc(p)) %>%
+    dplyr::filter(!is.na(newspaper)) 
+  if(!is.null(newspapers)){
+    plot_data <- 
+      plot_data %>%
+      filter(newspaper %in% newspapers)
+  }
+  plot_data <- plot_data %>% arrange(desc(p))
+  only_one <- length(newspapers) == 1
+  if(only_one){
+    plot_data$person <- factor(plot_data$person,
+                                  levels = plot_data$person)
+  }
+  if(!is.null(n)){
+    plot_data <- plot_data[1:n,]
+  }
+  
+  g <- ggplot(data = plot_data,
+              aes(x = person,
+                  y = p)) +
+    geom_bar(stat = 'identity',
+             alpha = 0.6,
+             fill = 'darkorange')
+  if(length(newspapers) != 1){
+    g <- g + facet_wrap(~newspaper)
+  } 
+  the_list <- 
+    paste0("adacolau, albanodante76, albert_rivera, albiol_xg, alejandrotgn, alevysoler, carlescampuzano, carmencalvo_, carrizosacarlos,\ncatencomu_podem, ciudadanoscs, ciutadanscs, compromis, cridanacional, cupnacional, enricmorera, espciudadana, esquerra_erc,\neva_granados, f_armengol, gabrielrufian, gabrielrufian_, inesarrimadas, isabelbonig, jeanmarcpujol, joanbaldovi, joanribo, joantarda,\njosepborrellf, junqueras, krls, marianorajoy, meritxell_batet, mespermallorca, mespermenorca, miqueliceta, monicaoltra, pablocasado_,\npdemocratacat, perearagones, ppcatalunya, ppopular, psoe, quimtorraipla, rogertorrent, sanchezcastejon, santi_abascal, socialistes_cat,\nsocialistesval, societatcc, toninoguera, vicentsolerm, vox_es, ximopuig")
+  if(language == 'en'){
+    x <- 'Twitter account'
+    title <- 'Distribution of newspaper references by politician (top 10)'
+    caption <- paste0('Includes the following twitter accounts only:\n', the_list)
+  } else {
+    x <- 'Compte twitter'
+    title <- 'Distribució de referencies a diaris per polític (top 10)'
+    caption <- paste0('Només inclou els comptes twitter següents:\n', the_list)
+    
+  }
+  subtitle <- ifelse(only_one, paste0('@', plot_data$newspaper[1]), '')
+  y <- 'Percentage'
+  
+  g <- g +
+    theme_vilaweb() +
+    labs(x = x, y = y, title = title, subtitle = subtitle, caption = caption) +
+    theme(plot.subtitle = element_text(size = 24, hjust = 0.5),
+          plot.caption = element_text(size = 7)) +
+    geom_text(aes(label = round(p, digits = 2)),
+              nudge_y = 0.3,
+              alpha = 0.6) +
+    theme(axis.text.x = element_text(angle = 90,
+                                     vjust = 0.5,
+                                     hjust = 1))
+  
+  return(g)
+}
+
