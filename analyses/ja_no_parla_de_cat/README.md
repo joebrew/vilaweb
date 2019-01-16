5f77eed575532827878f4d1dbac7751094681d05
Joe Brew <joebrew@gmail.com>
2019-01-16 17:39:57 +0545

    major update

---
+++ analyses/ja_no_parla_de_cat/README.md
@@ -0,0 +1,91 @@
+Who talks about Catalonia, when, and why
+================
+
+Introduction
+------------
+
+Some text goes here
+
+The questions
+-------------
+
+How much - and when - do Spanish national politicians with aspirations for Moncloa tweet about Catalonia?
+
+The methods
+-----------
+
+We gathered all publicly available tweets from the 5 leaders of the main Spanish political parties:
+
+-   Partido Popular: Pablo Casado (@pablocasado\_)
+-   VOX: Santiago Abascal (@santi\_abascal)
+-   Ciudadanos: Albert Tivera (@albert\_rivera)
+-   PSOE: Pedro Sánchez (@sanchezcastejon)
+-   Podemos: Pablo Iglesias (@pablo\_iglesias\_)
+
+This was a total of 79112 tweets. We then searched for the following four word-stems within all of these tweets:
+
+1.  "separatis" (includes separatista, separatismo, etc.)
+2.  "independ" (includes independentismo, independencia, independentista, etc.)
+3.  "catal" (includes cataluña, catalán, etc.)
+4.  "secesionis" (includes secesionismo, secesionista, etc.)
+5.  "golp" (includes golpe, golpista, golpismo, etc.)
+
+We aggregated data by week, tallied the total number of tweets containing any of these word-stems, and then calculated the percentage of all tweets that week.
+
+Results
+-------
+
+If we look at data for the entirety of 2018, we see major differences between the different politicians. The right-wing parties reference Catalonia frequently: 15.7% for Pablo Casado, 19.7% for Santi Abascal, and 26.1% for Albert Rivera.
+
+![](figures/unnamed-chunk-3-1.png)
+
+But examining the 2018 data in bulk hides important variations over time. As could be expected, the rate of tweets about Catalonia was highest during the fall 2017 referendum period and subsequent autonomous elections. However, even taking this into account, all five politicians show notable variation in the frequency with which they tweet about Catalonia. The below shows the weekly percentage of tweets of each politician.
+
+![](figures/unnamed-chunk-4-1.png)
+
+Perhaps the most drastic case of strange variation is that of the current President of the Government: Pedro Sánchez.
+
+### Pedro Sánchez
+
+The below shows Sánchez's weekly percentage of tweets referencing Catalonia.
+
+![](figures/unnamed-chunk-5-1.png)
+
+Immediately upon becoming PM, he stopped tweeting about Catalonia entirely for several weeks. Since resumption, the rate at which he tweets about Catalonia remains significantly lower than the rate prior to becoming PM.
+
+### Pablo Casado
+
+The below shows Casado's weekly percentage of tweets referencing Catalonia.
+
+![](figures/unnamed-chunk-6-1.png)
+
+Casado tweeted at an extremely high rate about Catalonia up until the December 21 2017 autonomous elections. Thereafter, the PP having experienced historically poor results, he changed subject. His rate of tweets about Catalonia remained low through the change to the Sánchez government. However, as soon as he became the leader of his party, he began tweeting about Catalonia again at a higher frequency.
+
+### Pablo Iglesias
+
+The below shows Iglesias' weekly percentage of tweets referencing Catalonia.
+
+![](figures/unnamed-chunk-7-1.png)
+
+What is most notable is the overall low rate with which Iglesias references Catalonia. Iglesias tweeted frequently about Catalonia during the referendum and election period, but has remained relatively silent on the matter since.
+
+### Albert Rivera
+
+Albert Rivera is the politican who most often references Catalonia. The below shows his weekly percentage of tweets which are about Catalonia.
+
+![](figures/unnamed-chunk-8-1.png)
+
+There is nothing notable about the variation over time. The most notable characteristic of Rivera's tweets about Catalonia is that they remain high throughout the entire period exmained (last 1.5 years).
+
+### Santiago Abascal
+
+The below shows the percentage of tweets about Catalonia from Santiago Abascal, leader of the newly emergent VOX party.
+
+![](figures/unnamed-chunk-9-1.png)
+
+Like Rivera, Abascal's rate of tweets about Catalonia does not vary much over time, remaining high during the entirety of the period studies.
+
+Catalan language plots
+----------------------
+
+![](figures/unnamed-chunk-10-1.png)![](figures/unnamed-chunk-10-2.png)![](figures/unnamed-chunk-10-3.png)![](figures/unnamed-chunk-10-4.png)![](figures/unnamed-chunk-10-5.png)![](figures/unnamed-chunk-10-6.png)![](figures/unnamed-chunk-10-7.png)
---
+++ analyses/ja_no_parla_de_cat/functions.R
@@ -0,0 +1,206 @@
+sanchez_plot <- function(language = 'en',
+                         person = 'Pedro Sánchez',
+                         yy = 31){
+  
+  people_df <- data_frame(person = c('Pedro Sánchez',
+                                     'Santiago Abascal',
+                                     'Pablo Casado',
+                                     'Pablo Iglesias',
+                                     'Albert Rivera'),
+                          account = c('sanchezcastejon',
+                                      'santi_abascal',
+                                      'pablocasado_',
+                                      'pablo_iglesias_',
+                                      'albert_rivera'))
+  account <- people_df$account[people_df$person == person]
+  
+  if(language == 'ca'){
+    x = 'Setmana'
+    y = 'Percentage de piulades de la setmana'
+    title = paste0('Piulades de ', person, ' mencionant Catalunya (i variacions)')
+    subtitle = 'Variacions: independen-tismo/tista/cia, separatis-ta/mo, catal-uña/an, secesionis-ta/mo, golp-ista/ismo'
+    caption = 'www.vilaweb.cat | Piulades públiques. Dades recollides el 16 de gener 2019.\nRegex utilitzat: "separatis|independ|catal|secesionis|golp". Joe Brew | @joethebrew'
+  } else {
+    x = 'Week'
+    y = 'Percentage of week\'s tweets'
+    title = paste0('Tweets from ', person, ' mentioning Catalonia (and variations)')
+    subtitle = 'Variations: independen-tismo/tista/cia, separatis-ta/mo, catal-uña/an, secesionis-ta/mo, golp-ista/ismo'
+    caption = 'Public tweets. Data collected on Jan 16 2019. Joe Brew | @joethebrew\nwww.vilaweb.cat Regex used: "separatis|independ|catal|secesionis|golp".'
+  }
+  pd <- tl %>%
+    mutate(date = vilaweb::date_truncate(date, 'week')) %>%
+    mutate(ind = grepl('separatis|independ|catal|secesionis|golp|secesionis|golp', tolower(tweet))) %>%
+    group_by(date,
+             person = username) %>%
+    summarise(n = n(),
+              p = length(which(ind)) / n() * 100) %>%
+    filter(date >= '2017-07-01')
+  left <- data_frame(date = sort(unique(pd$date)))
+  pd <- pd %>% filter(person == account)
+  pd <- left_join(left, pd)
+  pd$p[is.na(pd$p)] <- 0
+  dl <- range(pd$date)
+  dl <- date_truncate(dl, 'month')
+  dl <- seq(min(dl), max(dl), by = 'month')
+  ggplot(data = pd,
+         aes(x = date,
+             y = p)) +
+    geom_point(alpha = 0.6) +
+    geom_line(alpha =0.6) +
+    geom_area(alpha = 0.4) +
+    geom_vline(xintercept = as.Date('2018-06-01'),
+               lty = 2) +
+    geom_label(data = data.frame(date = as.Date('2018-06-01'),
+                               p = yy,
+                               label = 'Moción de\nCensura'),
+               aes(label = label,
+                   y = p)) +
+    labs(x = x,
+         y = y,
+         title = title,
+         subtitle = subtitle,
+         caption = caption) +
+    theme_vilaweb() +
+    theme(plot.subtitle = element_text(size = 10),
+          plot.title = element_text(size = 14)) +
+  scale_x_date(name = 'Setmana',
+               breaks = dl) +
+  theme(axis.text.x = element_text(angle = 90))
+}
+
+
+combined_plot <- function(language = 'en'){
+  
+  people_df <- data_frame(person = c('Pedro Sánchez',
+                                     'Santiago Abascal',
+                                     'Pablo Casado',
+                                     'Pablo Iglesias',
+                                     'Albert Rivera'),
+                          account = c('sanchezcastejon',
+                                      'santi_abascal',
+                                      'pablocasado_',
+                                      'pablo_iglesias_',
+                                      'albert_rivera'))
+  
+  if(language == 'ca'){
+    x = 'Setmana'
+    y = 'Percentage de piulades de la setmana'
+    title = paste0('Piulades de mencionant Catalunya (i variacions)')
+    subtitle = 'Variacions: independen-tismo/tista/cia, separatis-ta/mo, catal-uña/an, secesionis-ta/mo, golp-ista/ismo'
+    caption = 'www.vilaweb.cat | Piulades públiques. Dades recollides el 16 de gener 2019.\nRegex utilitzat: "separatis|independ|catal|secesionis|golp". Joe Brew | @joethebrew'
+  } else {
+    x = 'Week'
+    y = 'Percentage of week\'s tweets'
+    title = paste0('Tweets from mentioning Catalonia (and variations)')
+    subtitle = 'Variations: independen-tismo/tista/cia, separatis-ta/mo, catal-uña/an, secesionis-ta/mo, golp-ista/ismo'
+    caption = 'Public tweets. Data collected on Jan 16 2019. Joe Brew | @joethebrew\nwww.vilaweb.cat Regex used: "separatis|independ|catal|secesionis|golp".'
+  }
+  pd <- tl %>%
+    mutate(date = vilaweb::date_truncate(date, 'week')) %>%
+    mutate(ind = grepl('separatis|independ|catal|secesionis|golp|secesionis|golp', tolower(tweet))) %>%
+    group_by(date,
+             person = username) %>%
+    summarise(n = n(),
+              p = length(which(ind)) / n() * 100) %>%
+    filter(date >= '2017-07-01')
+  left <- expand.grid(date = sort(unique(pd$date)),
+                     person = sort(unique(pd$person)))
+  pd <- left_join(left, pd)
+  pd$p[is.na(pd$p)] <- 0
+  dl <- range(pd$date)
+  dl <- date_truncate(dl, 'month')
+  dl <- seq(min(dl), max(dl), by = 'month')
+  bp <- function(x,z){RColorBrewer::brewer.pal(n = 8,name =x)[z]}
+  
+  cols <- 
+    c(bp('Oranges', 6),
+      bp('Purples', 6),
+      bp('Blues', 6),
+      bp('Reds', 6),
+      bp('Greens', 6))
+  ggplot(data = pd,
+         aes(x = date,
+             y = p,
+             color = person)) +
+    # geom_point(alpha = 0.6) +
+    geom_line(alpha =0.9) +
+    geom_vline(xintercept = as.Date('2018-06-01'),
+               lty = 2) +
+    labs(x = x,
+         y = y,
+         title = title,
+         subtitle = subtitle,
+         caption = caption) +
+    theme_vilaweb() +
+    theme(plot.subtitle = element_text(size = 10),
+          plot.title = element_text(size = 14)) +
+    scale_x_date(name = 'Setmana',
+                 breaks = dl) +
+    theme(axis.text.x = element_text(angle = 90)) +
+    scale_color_manual(name = '',
+                       values = cols) +
+    theme(legend.text = element_text(size = 10))
+}
+
+plot_2018 <- function(language = 'en'){
+  
+  people_df <- data_frame(person = c('Pedro Sánchez',
+                                     'Santiago Abascal',
+                                     'Pablo Casado',
+                                     'Pablo Iglesias',
+                                     'Albert Rivera'),
+                          account = c('sanchezcastejon',
+                                      'santi_abascal',
+                                      'pablocasado_',
+                                      'pablo_iglesias_',
+                                      'albert_rivera'))
+  
+  if(language == 'ca'){
+    x = 'Setmana'
+    y = 'Percentage de piulades'
+    title = paste0('Piulades de mencionant Catalunya (i variacions)')
+    subtitle = 'Variacions: independen-tismo/tista/cia, separatis-ta/mo, catal-uña/an, secesionis-ta/mo, golp-ista/ismo'
+    caption = 'www.vilaweb.cat | Piulades públiques. Dades recollides el 16 de gener 2019.\nRegex utilitzat: "separatis|independ|catal|secesionis|golp". Joe Brew | @joethebrew'
+  } else {
+    x = 'Week'
+    y = 'Percentage of tweets'
+    title = paste0('Tweets from mentioning Catalonia (and variations)')
+    subtitle = 'Variations: independen-tismo/tista/cia, separatis-ta/mo, catal-uña/an, secesionis-ta/mo, golp-ista/ismo'
+    caption = 'Public tweets. Data collected on Jan 16 2019. Joe Brew | @joethebrew\nwww.vilaweb.cat Regex used: "separatis|independ|catal|secesionis|golp".'
+  }
+  pd <- tl %>%
+    filter(date >= '2018-01-01',
+           date <= '2018-12-31') %>%
+    mutate(ind = grepl('separatis|independ|catal|secesionis|golp|secesionis|golp', tolower(tweet))) %>%
+    group_by(person = username) %>%
+    summarise(n = n(),
+              p = length(which(ind)) / n() * 100) 
+  bp <- function(x,z){RColorBrewer::brewer.pal(n = 8,name =x)[z]}
+  
+  cols <- 
+    c(bp('Oranges', 6),
+      bp('Purples', 6),
+      bp('Blues', 6),
+      bp('Reds', 6),
+      bp('Greens', 6))
+  ggplot(data = pd,
+         aes(x = person,
+             y = p,
+             fill = person)) +
+    geom_bar(stat = 'identity') +
+    labs(x = x,
+         y = y,
+         title = title,
+         subtitle = subtitle,
+         caption = caption) +
+    theme_vilaweb() +
+    theme(plot.subtitle = element_text(size = 10),
+          plot.title = element_text(size = 14)) +
+    scale_fill_manual(name = '',
+                       values = cols) +
+    theme(legend.position = 'none') +
+    geom_text(aes(label = round(p, digits = 1)),
+              nudge_y = 2,
+              size = 5,
+              alpha = 0.7)
+}
